15e52e67c61822677fde8532ea9e5a116286f5f4
Wuilliam Lacruz <wuilliam.lacruz@pedidosya.com>
2019-09-08 13:36:19 -0300

    challenge progress

--- codility_01/src/index.js
+++ codility_01/src/index.js
@@ -1,3 +1,117 @@
-const test = (name) => `Hello World ${name}!`;
+function solution(K, M, A) {
+  function sort(a, b) {
+    if (a > b) {
+      return 1;
+    }
+    if (a < b) {
+      return -1;
+    }
+    return 0;
+  }
+  function getLead(list) {
+    const numbers = list.reduce((acc, v) => {
+      acc[v] = (acc[v] + 1) || 1;
+      return acc;
+    }, {});
+    const twoLeads = Object.entries(numbers)
+      .filter(([n, v]) => v === list.length / 2).map(([n]) => Number(n));
+    const thereAreTwoLeads = twoLeads.length > 0;
+    if (thereAreTwoLeads) {
+      return twoLeads;
+    }
+    const [lead, count] = Object.entries(numbers).reduce(([a, b], [k, v]) => {
+      if (b > v) {
+        return [a, b];
+      }
+      return [Number(k), Number(v)];
+    }, [0, 0]);
+    const thereIsLead = lead > 0 && (count > Math.floor((list.length / 2)));
+    console.log(lead, count, list.length, thereIsLead);
+    return (thereIsLead) ? lead : false;
+    // const counts = { lead: -1, count: -1 };
+    // const numbers = {};
+    // list.forEach((item) => {
+    //   const count = list.filter((i) => i === item).length;
+    //   numbers[item] = count;
+    //   if (numbers[item] > counts.count) {
+    //     counts.lead = item;
+    //     counts.count = count;
+    //   }
+    // });
 
-const a = test('Mary');
+    // const values = Object.values(numbers).sort(sort);
+    // const max = values[0];
+    // const existsLead = !!values.find((i) => i >= max);
+    // if (!existsLead) {
+    //   return false;
+    // }
+
+    // return counts.lead;
+  }
+
+  function getNewSegment(list, start, end) {
+    start += 1;
+    const newA = list.slice(0);
+    const qty = end + start;
+    // console.log(`${list.length}, ${start}, ${qty}, long=${end}`);
+    if (list.length >= qty) {
+      for (let i = start; i < qty; i += 1) {
+        newA[i] += 1;
+      }
+      console.log('SEG', newA);
+      return newA;
+    }
+    return false;
+  }
+
+  // write your code in JavaScript (Node.js 8.9.4)
+  if (!K
+    || !M
+    || !A
+    || !Number.isInteger(K)
+    || !Number.isInteger(M)
+    || (A.length > 100000)
+    || (A.length < 1)
+    || (M > 100000)
+    || (M < 1)
+    || (K > 100000)
+    || (K < 1)) {
+    return [];
+  }
+  const outOfRangeItems = !!A.find((i) => i > 100000
+    || i < 1
+    || i > M);
+  if (outOfRangeItems) {
+    return [];
+  }
+  if (A.length === 1) {
+    return [A[0] + 1];
+  }
+
+  let leads = [];
+
+  let thereIsLead = true;
+  let count = 0;
+  let newA = A.slice(0);
+  console.log('LIST', newA);
+  while (thereIsLead) {
+    if (newA) {
+      const newLead = getLead(newA);
+      if (newLead && newLead instanceof Array) {
+        leads = leads.concat(newLead);
+      }
+      if (typeof (newLead) === 'number') {
+        leads.push(newLead);
+      }
+      newA = getNewSegment(A.slice(0), count, K);
+    } else {
+      thereIsLead = false;
+    }
+    count += 1;
+  }
+  const uniqueLeads = (v, i, a) => a.indexOf(v) === i;
+  return leads.filter(uniqueLeads).sort(sort);
+}
+
+
+export default solution;
--- codility_01/tests/codility01.spec.js
+++ codility_01/tests/codility01.spec.js
@@ -1,8 +1,67 @@
 /* eslint-env jest */
+import solution from '../src/index';
+
 
 describe('Given', () => {
-  it('Test', () => {
-    expect(true).toEqual(true);
-    expect('test').toEqual('test');
+  it('empty', () => {
+    const result = solution(0, 0, []);
+    expect(result).toEqual([]);
+  });
+
+  it('test 1', () => {
+    const result = solution(3, 5, [2, 1, 3, 1, 2, 2, 3]);
+    expect(result).toEqual([2, 3]);
+  });
+
+  it('test 2', () => {
+    const result = solution(3, 7, [1, 1, 1, 1, 2, 2, 2, 2, 3]);
+    expect(result).toEqual([2]);
+  });
+
+  it('test 3', () => {
+    const result = solution(3, 5, [2, 2, 4, 2, 2, 2, 3]);
+    expect(result).toEqual([2, 3]);
+  });
+
+  it('test 4', () => {
+    const result = solution(3, 5, [2, 1, 3, 2, 3, 3, 3]);
+    expect(result).toEqual([3]);
+  });
+
+  it('test 5', () => {
+    const result = solution(4, 2, [1, 2, 2, 1, 2]);
+    expect(result).toEqual([2, 3]);
+  });
+
+  it('test 6', () => {
+    const result = solution(1, 1, [1]);
+    expect(result).toEqual([2]);
+  });
+
+  it('test 7', () => {
+    const result = solution(1, 100000, [100000]);
+    expect(result).toEqual([100001]);
+  });
+
+  it('test 8', () => {
+    const result = solution(1, 100000, [99999, 99999, 100000, 100000]);
+    expect(result).toEqual([99999, 100000]);
+  });
+
+  it('test 9 one lead', () => {
+    const result = solution(1, 4, [1]);
+    expect(result).toEqual([2]);
+  });
+
+  it('test 10', () => {
+    const records = 99;
+    const list = [...Array(records).keys()];
+    const result = solution(1, 1, list);
+    expect(result).toEqual([]);
+  });
+
+  it('test 10 half', () => {
+    const result = solution(4, 8, [2, 2, 4, 4]);
+    expect(result).toEqual([2, 4]);
   });
 });
